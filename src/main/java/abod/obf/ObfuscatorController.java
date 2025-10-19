35747df7695ca50c6394524e672838f5782bf096
Abodx9 <abodx@gmx.de>
2025-10-19 04:47:04 +0200

    Endpoint to encode python string (String should be in the body)

--- src/main/java/abod/obf/ObfuscatorController.java
+++ src/main/java/abod/obf/ObfuscatorController.java
@@ -2,11 +2,19 @@ package abod.obf;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class ObfuscatorController {
+ 
   @GetMapping("/")
   public String main() {
     return "Hey! I am the Obfuscator!";
+  }
+
+
+  @GetMapping("/encode-py")
+  public String obfPy(@RequestBody String input) {
+    return Tricks.encode_Py(input);
   }
 }
