bf11228ae0dc7aa1b4a615601fb458aebfb5d516
Abodx9 <abodx@gmx.de>
2025-10-19 03:30:55 +0200

    Init Spring Boot App

---
+++ src/main/java/abod/obf/ObfxApplication.java
@@ -0,0 +1,13 @@
+package abod.obf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class obfxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(obfxApplication.class, args);
+	}
+
+}
