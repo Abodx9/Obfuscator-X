1b711499abf77983fcda5f8052c4b4ccc205d601
Abodx9 <abodx@gmx.de>
2025-10-19 03:51:36 +0200

    Fix the class name

--- src/main/java/abod/obf/ObfxApplication.java
+++ src/main/java/abod/obf/ObfxApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class obfxApplication {
+public class ObfxApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(obfxApplication.class, args);
+		SpringApplication.run(ObfxApplication.class, args);
 	}
 
 }
